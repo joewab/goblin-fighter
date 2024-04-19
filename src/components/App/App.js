4a0d68cdf786267e499d57bc8d8db291f819cc1d
Joe AB <joe.anthonybrown@gmail.com>
2024-04-18 20:32:37 -0500

    hooked up api and added some styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,36 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  useEffect(() => {
+  }, [])
+
+  const [currentMonster, setCurrentMonster] = useState('');
+
+  function getMonsters(){
+    axios({
+      method: 'GET',
+      url: 'https://www.dnd5eapi.co/api/monsters/goblin'
+    }).then((response) => {
+      console.log('response');
+      console.log(response);
+      setCurrentMonster(response.data.index);
+    })}
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Goblin Fighter
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body className='App-body'>
+      <h4>get a monster:</h4>
+      <button onClick={getMonsters}> Monster! </button>
+      <h4>You got a {currentMonster}!</h4>
+      </body>
     </div>
   );
 }
